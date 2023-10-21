8f0ed0dc2a7170e9795c1f9ba372044af3f298cc
yone-chan <142673364+yone-chan@users.noreply.github.com>
2023-10-22 06:58:35 +0900

    更新
    ・出力する数値の変更
    ・ifとelse if の条件式の変更

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,16 +1,17 @@
-let num = Math.floor(Math.random() * 16);
+let num = Math.floor(Math.random() * 100);
+num += 1;
 console.log(num);
 
-if (num === 3 || num === 6 || num === 9 || num === 12) {
-  console.log('3の倍数です');
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
 }
 
-else if (num === 5 || num === 10) {
+else if (num % 5 === 0) {
   console.log('5の倍数です');
 }
 
-else if (num === 15) {
-  console.log('3と5の倍数です');
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
 }
 
 else {
